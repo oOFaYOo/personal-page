8d96806bc987e10447ff6c5b88a383a1d9d76bbe
Nika Ts <nika.tseleva@mail.ru>
2023-03-08 08:37:47 +0400

    remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className={'bg-amber-300 overflow-y-auto relative w-full h-full'}>
+    <div className={'overflow-y-auto relative w-full h-full'}>
       <Header />
 
     </div>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -12,9 +12,8 @@ import {setTheme} from "../../store/slice";
 
 const Header = () => {
 
-    const windowWidth = window.screen.availWidth;
     const dispatch = useDispatch();
-    const {theme} = useSelector((state:RootState) => state.PortfolioLandingPageStore)
+    const {theme} = useSelector((state: RootState) => state.PortfolioLandingPageStore)
     const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
 
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -29,73 +28,86 @@ const Header = () => {
 
     return (
         <header className={'text-xl w-full sm:h-[10vh] lg:h-[10vh] h-[10%] flex flex-row justify-between'}>
-            <div className={`${windowWidth > 1024 ? 'w-[40%]' : 'w-[30%]'} flex h-full justify-evenly items-center`}>
-                {windowWidth < 1024
-                    ? <>
-                        <button onClick={(e)=>{handleClick(e)}}>
-                        <DehazeIcon sx={{ fontSize: windowWidth > 640 ? '2.5rem' : '4.5rem' }} />
-                        </button>
-                        <Popover
-                            open={open}
-                            anchorEl={anchorEl}
-                            onClose={handleClose}
-                            anchorOrigin={{
-                                vertical: 'bottom',
-                                horizontal: 'left',
-                            }}
-                        >
-                            <div className={`${windowWidth > 640 ? 'text-3xl' : 'text-6xl'} flex flex-col`}>
-                                <a className={'mx-2 my-4'}>Skills</a>
-                                <a className={'mx-2 my-4'}>Portfolio</a>
-                                <a className={'mx-2 my-4'}>Experience</a>
-                            </div>
-                        </Popover>
-                    </>
-                    : <>
-                        <a>Skills</a>
-                        <a>Portfolio</a>
-                        <a>Experience</a>
-                      </>
-                }
+            <div className={'lg:hidden w-[30%] flex h-full justify-evenly items-center'}>
+                <button onClick={(e) => {
+                    handleClick(e)
+                }}>
+                    <DehazeIcon sx={{fontSize: '4rem'}}/>
+                </button>
+                <Popover
+                    open={open}
+                    anchorEl={anchorEl}
+                    onClose={handleClose}
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'left',
+                    }}
+                    PaperProps={{
+                        style: {
+                            borderRadius: '16px',
+                            padding: '4px'
+                        }
+                    }}
+                    className={'lg:hidden'}
+                >
+                    <div className={'text-5xl flex flex-col'}>
+                        <a className={'mx-2 my-4'}>Skills</a>
+                        <a className={'mx-2 my-4'}>Portfolio</a>
+                        <a className={'mx-2 my-4'}>Experience</a>
+                    </div>
+                </Popover>
             </div>
-            <div className={`${windowWidth > 1024 ? 'w-[30%]' : 'w-[40%]'} flex h-full justify-evenly items-center`}>
+            <div className={'lg:flex w-[40%] hidden h-full justify-evenly items-center'}>
+                <a>Skills</a>
+                <a>Portfolio</a>
+                <a>Experience</a>
+            </div>
+            <div className={'w-[30%] lg:flex hidden h-full justify-evenly items-center'}>
                 <a href={'https://t.me/nikatseleva'} target="_blank">
-                    <TelegramIcon sx={{ fontSize: windowWidth > 640 ? '2rem' : '4.5rem' }} />
+                    <TelegramIcon sx={{fontSize: '2rem'}}/>
                 </a>
                 <a href={'https://github.com/oOFaYOo'} target="_blank">
-                    <GitHubIcon sx={{ fontSize: windowWidth > 640 ? '2rem' : '4.5rem' }} />
+                    <GitHubIcon sx={{fontSize: '2rem'}}/>
                 </a>
                 <a href={"mailto:nikatseleva@gmail.com"} target="_blank">
-                    <AlternateEmailIcon sx={{ fontSize: windowWidth > 640 ? '2rem' : '4.5rem' }} />
+                    <AlternateEmailIcon sx={{fontSize: '2rem'}}/>
                 </a>
             </div>
-            <div className={'flex w-[30%] h-full justify-center items-center'}>
+            <div className={'w-[40%] lg:hidden flex h-full justify-evenly items-center'}>
+                <a href={'https://t.me/nikatseleva'} target="_blank">
+                    <TelegramIcon sx={{fontSize: '4rem'}}/>
+                </a>
+                <a href={'https://github.com/oOFaYOo'} target="_blank">
+                    <GitHubIcon sx={{fontSize: '4rem'}}/>
+                </a>
+                <a href={"mailto:nikatseleva@gmail.com"} target="_blank">
+                    <AlternateEmailIcon sx={{fontSize: '4rem'}}/>
+                </a>
+            </div>
+            <div className={'lg:flex hidden w-[30%] h-full justify-center items-center'}>
+                <LightModeIcon sx={{fontSize: '2rem'}}/>
+                <Switch id={'theme-switcher'} checked={theme !== 'light'} onChange={(e) => {
+                    const theme = e.currentTarget.checked ? 'dark' : 'light';
+                    // localStorage.setItem('theme', theme)
+                    dispatch(setTheme(theme));
+                }}/>
+                <DarkModeIcon sx={{fontSize: '2rem'}}/>
+            </div>
+            <div className={'lg:hidden flex w-[30%] h-full justify-center items-center'}>
                 {
-                    windowWidth > 640
-                    ? <>
-                        <LightModeIcon sx={{ fontSize: '2rem' }} />
-                        <Switch id={'theme-switcher'} checked={theme !== 'light'} onChange={(e)=>{
-                            const theme = e.currentTarget.checked ? 'dark' : 'light';
-                            // localStorage.setItem('theme', theme)
-                            dispatch(setTheme(theme));
-                        }} />
-                        <DarkModeIcon sx={{ fontSize: '2rem' }} />
-                      </>
-                    : (
-                        theme === 'dark'
-                            ? <LightModeIcon sx={{ fontSize: '4.5rem' }} onClick={
-                                ()=>{
-                                    // localStorage.setItem('theme', 'light')
-                                    dispatch(setTheme('light'));
-                                }
-                            } />
-                            : <DarkModeIcon sx={{ fontSize: '4.5rem' }} onClick={
-                                ()=>{
-                                    // localStorage.setItem('theme', 'dark')
-                                    dispatch(setTheme('dark'));
-                                }
-                            } />
-                        )
+                    theme === 'dark'
+                        ? <LightModeIcon sx={{fontSize: '4rem'}} onClick={
+                            () => {
+                                // localStorage.setItem('theme', 'light')
+                                dispatch(setTheme('light'));
+                            }
+                        }/>
+                        : <DarkModeIcon sx={{fontSize: '4rem'}} onClick={
+                            () => {
+                                // localStorage.setItem('theme', 'dark')
+                                dispatch(setTheme('dark'));
+                            }
+                        }/>
                 }
             </div>
         </header>

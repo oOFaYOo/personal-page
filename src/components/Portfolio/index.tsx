a169a598a88209c7f041dfdc62e7a6c897e706ae
Nika Ts <nika.tseleva@mail.ru>
2023-03-09 15:28:25 +0400

    fix visual bug

--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -10,13 +10,10 @@ const Portfolio = () => {
             <p className={'w-full mb-6 text-center lg:text-3xl text-4xl font-bold'}>Portfolio</p>
             <div className={'lg:px-24 py-4 w-full gap-4 flex lg:flex-row flex-col justify-evenly items-center'}>
                 <div
-                    className={'lg:h-[90%] min-h-[450px] h-[35%] p-6 w-[80%] lg:w-[45%] rounded-xl flex flex-col items-center justify-between overflow-hidden bg-neutral-300'}>
+                    className={'lg:h-full relative min-h-[450px] h-[35%] p-6 w-[80%] lg:w-[45%] rounded-xl flex flex-col items-center justify-between overflow-hidden bg-neutral-300'}>
                     <img className={'rounded-xl w-full lg:mb-4'} src={demo1}/>
                     <p className={'text-justify lg:inline hidden'}>
-                        A small project that uses a fake backend, allows you to register and
-                        create your own goals for the year, add steps to complete the goal. Goals can be deleted,
-                        steps can be edited, deleted and added. For the convenience of selecting goals,
-                        there is sorting, searching and filtering.
+                        The application allows you to create goals for the year, add, delete, edit them and track progress
                     </p>
                     <div className={'flex w-full grow lg:grow-0 flex-row justify-evenly items-center mt-4'}>
                         <a href={'https://yearly-goal-tracker.vercel.app/yearly_goal_tracker/auth'}
@@ -30,10 +27,10 @@ const Portfolio = () => {
                     </div>
                 </div>
                 <div
-                    className={'lg:h-[90%] min-h-[450px] h-[35%] p-6 w-[80%] lg:w-[45%] rounded-xl flex flex-col items-center justify-between overflow-hidden bg-neutral-300'}>
+                    className={'lg:h-full relative min-h-[450px] h-[35%] p-6 w-[80%] lg:w-[45%] rounded-xl flex flex-col items-center justify-between overflow-hidden bg-neutral-300'}>
                     <img className={'rounded-xl w-full lg:mb-4'} src={demo2}/>
                     <p className={'text-justify lg:inline hidden'}>
-                        A small personal project that simulates an application for some kind of business.
+                        A small personal project that simulates an application for some kind of business
                     </p>
                     <div className={'flex w-full grow lg:grow-0 flex-row justify-evenly items-center mt-4'}>
                         <a href={'https://analytical-service.vercel.app/users'}

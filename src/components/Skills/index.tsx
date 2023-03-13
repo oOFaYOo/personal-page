b9cd5aee0af307eb1dc5a77f03bde8d060195342
Nika Ts <nika.tseleva@mail.ru>
2023-03-13 17:25:17 +0400

    edit style for mobile, remove unused code

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,4 +1,8 @@
 import React, {useRef} from "react";
+import {useSelector} from "react-redux";
+import {RootState} from "../../store";
+import {useOnScreen} from "../../App";
+
 import image1 from '../../images/1.png'
 import image2 from '../../images/2.png'
 import image3 from '../../images/3.png'
@@ -8,49 +12,62 @@ import image6 from '../../images/6.png'
 import image7 from '../../images/7.png'
 import image8 from '../../images/8.png'
 import image9 from '../../images/9.png'
-import {useSelector} from "react-redux";
-import {RootState} from "../../store";
-import {useOnScreen} from "../../App";
+
+function divider(arr: { skill: string, image: any }[]) {
+    const result: { skill: string, image: any }[][] = [[], [], []];
+    arr.forEach((v, i) => {
+        if (result[0].length !== 3) {
+            result[0].push(v);
+        } else if (result[1].length !== 3) {
+            result[1].push(v);
+        } else {
+            result[2].push(v);
+        }
+    })
+    return result;
+}
 
 const Skills = () => {
 
-    const {theme} = useSelector((state: RootState) => state.PortfolioLandingPageStore)
+    const {theme} = useSelector((state: RootState) => state.PortfolioLandingPageStore);
 
-    const skills = [['CSS', 'HTML', 'TailwindCSS'], ['JavaScript', 'TypeScript', 'React'], ['React Testing Library', 'Jest', 'Redux']];
-    const images = [[image1, image2, image3], [image4, image5, image6], [image7, image8, image9]];
-    // const skillsForMobile = [
-    //     {skill:'CSS', image:image1},
-    //     {skill:'HTML', image:image2},
-    //     {skill:'TailwindCSS', image:image3},
-    //     {skill:'JavaScript', image:image4},
-    //     {skill:'TypeScript', image:image5},
-    //     {skill:'React', image:image6},
-    //     {skill:'React Testing Library', image:image7},
-    //     {skill:'Jest', image:image8},
-    //     {skill:'Redux', image:image9}
-    // ];
+    const skills = [
+        {skill: 'CSS', image: image1},
+        {skill: 'HTML', image: image2},
+        {skill: 'TailwindCSS', image: image3},
+        {skill: 'JavaScript', image: image4},
+        {skill: 'TypeScript', image: image5},
+        {skill: 'React', image: image6},
+        {skill: 'React Testing Library', image: image7},
+        {skill: 'Jest', image: image8},
+        {skill: 'Redux', image: image9}
+    ];
 
-    const ref = useRef<HTMLDivElement>(null)
-    const isVisible = useOnScreen(ref)
+    const ref = useRef<HTMLDivElement>(null);
+    const isVisible = useOnScreen(ref);
 
     return (
-        <div className={`${theme === 'light' ? 'text-neutral-900' : 'text-neutral-300'} 
-        mb-12 mt-36 px-24 flex flex-col justify-center items-center`}>
-            <a id={'skills-anchor'} />
-            <p className={`my-6 lg:text-3xl pt-4 text-4xl font-bold ${isVisible ? 'anim-active' : 'anim translate-x-[-30%]'}`}>Skills</p>
-            <div ref={ref} className={`flex flex-col justify-center gap-2 items-center w-full ${isVisible ? 'anim-active' : 'anim translate-y-[20%]'}`}>
-                {skills.map((value,index)=>{
+        <div ref={ref} className={`${theme === 'light' ? 'text-neutral-900' : 'text-neutral-300'} 
+        mb-12 px-24`}>
+            <a id={'skills-anchor'}/>
+            <h2 className={`mobile-headers text-center font-bold mb-9 lg:text-3xl text-4xl 
+            ${isVisible ? 'anim-active' : 'anim translate-x-[-10%]'}`}>Skills</h2>
+            <div className={`mobile-invisible flex flex-col items-center gap-2 w-full 
+            ${isVisible ? 'anim-active' : 'anim translate-y-[20%]'}`}>
+                {divider(skills).map((value, index) => {
                     return (
                         <div className={'flex w-full flex-row gap-2 justify-evenly items-center'} key={index}>
-                            {value.map((skill, i)=>{
+                            {value.map((skill, i) => {
                                 return (
                                     <div key={i}
                                          className={`${theme === 'light' ? 'bg-neutral-200/30' : 'bg-neutral-800/60'} 
                                          lg:w-[30%] lg:h-[200px] w-[250px] p-2 whitespace-normal shadow 
                                          h-[250px] text-2xl text-center font-semibold rounded-xl flex 
                                          flex-col justify-evenly items-center`}>
-                                        {skill}
-                                        <img className={`${theme === 'light' ? 'opacity-20' : 'opacity-90'} relative h-[130px] w-[130px]`} src={images[index][i]} />
+                                        {skill.skill}
+                                        <img
+                                            className={`${theme === 'light' ? 'opacity-20' : 'opacity-90'} h-[130px] w-[130px]`}
+                                            src={skill.image}/>
                                     </div>
                                 )
                             })}
@@ -58,19 +75,23 @@ const Skills = () => {
                     )
                 })}
             </div>
-            {/*<div className={'lg:hidden max-w-full border-neutral-300 border-4 flex flex-row relative rounded-xl justify-start gap-2 items-center overflow-x-scroll'}>*/}
-            {/*    {*/}
-            {/*        skillsForMobile.map((v, i)=>{*/}
-            {/*            return (*/}
-            {/*                <div key={i}*/}
-            {/*                    className={'min-w-[250px] h-[250px] text-center text-2xl font-semibold rounded-xl flex flex-col justify-evenly items-center bg-neutral-200'}>*/}
-            {/*                    {v.skill}*/}
-            {/*                    <img className={'relative h-[130px] w-[130px]'} src={v.image} />*/}
-            {/*                </div>*/}
-            {/*            )*/}
-            {/*        })*/}
-            {/*    }*/}
-            {/*</div>*/}
+            <div className={'mobile-visible flex-col items-center text-[3rem] hidden relative w-full'}>
+                {
+                    skills.map((v, i) => {
+                        return (
+                            <div key={i}
+                                 className={`${theme === 'light' ? 'bg-neutral-200/30' : 'bg-neutral-800/60'} 
+                                w-full mb-4 h-[150px] px-12 text-center font-semibold whitespace-normal rounded-xl 
+                                flex flex-row justify-between items-center shadow`}>
+                                {v.skill}
+                                <img
+                                    className={`${theme === 'light' ? 'opacity-20' : 'opacity-90'} h-[100px] w-[100px]`}
+                                    src={v.image}/>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 };

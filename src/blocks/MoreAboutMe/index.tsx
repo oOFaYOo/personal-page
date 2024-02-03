7575b8b03234adade466204e95f90fd6d3f231e3
Nika Ts <nika.tseleva@mail.ru>
2024-02-03 09:19:57 +0400

    upd style for mobile

--- src/blocks/MoreAboutMe/index.tsx
+++ src/blocks/MoreAboutMe/index.tsx
@@ -18,23 +18,23 @@ const MoreAboutMe = ({openMoreInfo}: { openMoreInfo: Dispatch<SetStateAction<boo
     return (
         <div className={`${theme === 'light' ? 'text-neutral-900' : 'text-neutral-300'} 
         fixed z-40 bg-inherit w-full h-full flex justify-center items-center`}>
-            <div className={'flex flex-col justify-center items-center mobile:w-[70%] w-[50%] lg:max-w-[500px]'}>
-                <div className={'w-full mb-8'}>
+            <div className={'flex flex-col justify-center items-center mobile:w-[80%] w-[50%] lg:max-w-[500px]'}>
+                <div className={'w-full mb-8 mobile:mb-32'}>
                     <ArrowBackIcon onClick={() => {
                         openMoreInfo(false)
                     }} className={'hover:cursor-pointer hover:opacity-100 opacity-50'}/>
                 </div>
                 <div>
-                    <h1 className={'font-mono mobile:text-8xl text-6xl lg:text-4xl text-center mb-8 tracking-wide'}>
+                    <h1 className={'font-mono mobile:text-8xl text-6xl lg:text-4xl text-center mobile:mb-16 mb-8 tracking-wide'}>
                         Experience
                     </h1>
-                    <h2 className={'mobile:text-5xl text-3xl lg:text-xl italic font-bold'}>
+                    <h2 className={'mobile:text-5xl text-3xl lg:text-xl italic font-bold mobile:mb-8'}>
                         Frontend Web Developer in PJSC SberBank
                     </h2>
-                    <h3 className={'mobile:text-5xl text-3xl lg:text-xl font-mono mb-4 tracking-tight'}>
+                    <h3 className={'mobile:text-5xl text-3xl lg:text-xl font-mono mb-4 tracking-tight mobile:mb-12'}>
                         09/2021 – 08/2023
                     </h3>
-                    <p className={'mobile:text-5xl text-3xl lg:text-xl font-sans mb-8 text-justify'}>
+                    <p className={'mobile:text-5xl text-3xl lg:text-xl font-sans mb-8 mobile:mb-12 text-justify'}>
                         Implementation of an infrastructure analytical microservice using
                         layouts from Figma. Participation in API design, work in a cross-functional
                         team, integration testing and bug fixing.
@@ -49,7 +49,7 @@ const MoreAboutMe = ({openMoreInfo}: { openMoreInfo: Dispatch<SetStateAction<boo
                             )
                         }
                     </div>
-                    <div className={'flex justify-center items-center mt-16 mobile:text-5xl text-3xl lg:text-xl'}>
+                    <div className={'flex justify-center items-center mobile:mt-32 mt-16 mobile:text-5xl text-3xl lg:text-xl'}>
                         <a
                             href={cv}
                             target="_blank"
